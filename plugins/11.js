234d7d525826351bbe5afba6f93586aec91ed6d6
PROYECTOEBG <156557399+PROYECTOEBG@users.noreply.github.com>
2024-02-04 12:08:35 -0500

    Create 11.js

---
+++ plugins/11.js
@@ -0,0 +1,23 @@
+import PhoneNumber from 'awesome-phonenumber'
+let handler = async (m, { conn, __dirname, isRowner, isOwner, isBotAdmin, usedPrefix, groupMetadata, groups, _package, participants }) => { 
+let blockedUser = global.db.data.blockedUsers && global.db.data.blockedUsers[i] ? global.db.data.blockedUsers[i] : [];
+const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
+}
+
+let txt
+const chats = Object.entries(conn.chats).filter(([jid, data]) => jid && data.isChats)
+groups = Object.values(await conn.groupFetchAllParticipating())
+txt = `ESTOY EN ESTOS GRUPOS ✅\n`
+for (let i = 0; i < groups.length; i++) {
+txt += ` 
+*GRUPO*: ${groups[i].subject}
+*ID:* ${groups[i].id}\n`
+}
+m.reply(txt.trim())
+}
+handler.help = ['groups', 'grouplist']
+handler.tags = ['info']
+handler.command = /^(grupos)$/i
+handler.rowner = true;
+handler.exp = 30
+export default handler
